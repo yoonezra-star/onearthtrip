46f3f76ca072afbd6c201fe27660e47e03e90c38
yoonezra-star <yoonezra@gmail.com>
2026-08-25 22:42:13 +0900

    Add Al Wathba story cluster data

---
+++ src/data/alWathbaStories.ts
@@ -0,0 +1,7 @@
+export const alWathbaStoryPermalinks = [
+  "/2026/08/al-wathba-wetland-reserve-guide.html",
+  "/2026/08/al-wathba-fossil-dunes-guide.html",
+  "/2026/08/al-wathba-camel-race-guide.html",
+  "/2026/08/al-wathba-cycle-track-guide.html",
+  "/2026/08/al-wathba-day-trip-guide.html"
+] as const;
