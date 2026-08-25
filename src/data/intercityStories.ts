b83ce805a3533f761213f4f11ee1f949678cab70
yoonezra-star <yoonezra@gmail.com>
2026-08-25 21:35:33 +0900

    Add Abu Dhabi Dubai intercity story cluster

---
+++ src/data/intercityStories.ts
@@ -0,0 +1,7 @@
+export const intercityStoryPermalinks = [
+  "/2026/08/abu-dhabi-dubai-bus-guide.html",
+  "/2026/08/abu-dhabi-dubai-taxi-cost-guide.html",
+  "/2026/08/abu-dhabi-dubai-rental-car-guide.html",
+  "/2026/08/zayed-airport-dubai-transport-guide.html",
+  "/2026/08/dubai-day-trip-from-abu-dhabi-guide.html"
+] as const;
