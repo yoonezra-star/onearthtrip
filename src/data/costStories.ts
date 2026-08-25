dd34d8f69812a6be388784167b81759b862bcd12
yoonezra-star <yoonezra@gmail.com>
2026-08-25 18:32:49 +0900

    Define Abu Dhabi travel cost cluster

---
+++ src/data/costStories.ts
@@ -0,0 +1,7 @@
+export const costStoryPermalinks = [
+  "/2026/08/abu-dhabi-airport-city-transport-cost.html",
+  "/2026/08/downtown-yas-island-transport-cost.html",
+  "/2026/08/downtown-saadiyat-transport-cost.html",
+  "/2026/08/abu-dhabi-3-day-travel-budget.html",
+  "/2026/08/abu-dhabi-5-day-travel-budget.html"
+] as const;
