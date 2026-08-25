3253069853d453e6073e740fbf84f16cd1e4e55b
yoonezra-star <yoonezra@gmail.com>
2026-08-25 15:02:36 +0900

    Add Abu Dhabi attraction guide cluster

---
+++ src/data/attractionStories.ts
@@ -0,0 +1,7 @@
+export const attractionStoryPermalinks = [
+  "/2026/08/louvre-abu-dhabi-visit-guide.html",
+  "/2026/08/zayed-national-museum-visit-guide.html",
+  "/2026/08/natural-history-museum-abu-dhabi-guide.html",
+  "/2026/08/teamlab-phenomena-abu-dhabi-guide.html",
+  "/2026/08/yas-island-theme-park-choice-guide.html"
+] as const;
