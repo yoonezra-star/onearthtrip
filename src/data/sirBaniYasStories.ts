a840a875368e7ee5352ccf56587c0d0a93950afd
yoonezra-star <yoonezra@gmail.com>
2026-08-25 22:25:49 +0900

    Add Sir Bani Yas story cluster data

---
+++ src/data/sirBaniYasStories.ts
@@ -0,0 +1,7 @@
+export const sirBaniYasStoryPermalinks = [
+  "/2026/08/sir-bani-yas-island-getting-there-guide.html",
+  "/2026/08/jebel-dhanna-sir-bani-yas-boat-guide.html",
+  "/2026/08/sir-bani-yas-wildlife-safari-guide.html",
+  "/2026/08/sir-bani-yas-island-overnight-itinerary.html",
+  "/2026/08/al-dhafra-coast-sir-bani-yas-guide.html"
+] as const;
