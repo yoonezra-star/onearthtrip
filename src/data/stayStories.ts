df5c8222b3b1f1adf4ead026368aa8a7355b7678
yoonezra-star <yoonezra@gmail.com>
2026-08-25 16:47:43 +0900

    Define Abu Dhabi stay cluster

---
+++ src/data/stayStories.ts
@@ -0,0 +1,7 @@
+export const stayStoryPermalinks = [
+  "/2026/08/abu-dhabi-where-to-stay-area-guide.html",
+  "/2026/08/corniche-downtown-where-to-stay-guide.html",
+  "/2026/08/yas-island-where-to-stay-guide.html",
+  "/2026/08/saadiyat-island-where-to-stay-guide.html",
+  "/2026/08/zayed-airport-nearby-where-to-stay-guide.html"
+] as const;
