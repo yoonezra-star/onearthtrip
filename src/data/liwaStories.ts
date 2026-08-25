0ab8f90c9f955b2c73d1f747a355aa490cdbb3e6
yoonezra-star <yoonezra@gmail.com>
2026-08-25 22:11:31 +0900

    Add Liwa cluster data

---
+++ src/data/liwaStories.ts
@@ -0,0 +1,7 @@
+export const liwaStoryPermalinks = [
+  "/2026/08/abu-dhabi-liwa-bus-guide.html",
+  "/2026/08/abu-dhabi-liwa-rental-car-guide.html",
+  "/2026/08/liwa-oasis-visit-guide.html",
+  "/2026/08/liwa-desert-driving-safety-guide.html",
+  "/2026/08/liwa-day-overnight-itinerary.html"
+] as const;
