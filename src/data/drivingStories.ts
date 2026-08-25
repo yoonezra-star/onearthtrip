029a145fe1fb1bb8e6cf798f6ffd9db5825685fe
yoonezra-star <yoonezra@gmail.com>
2026-08-25 21:25:08 +0900

    Add Abu Dhabi driving story cluster

---
+++ src/data/drivingStories.ts
@@ -0,0 +1,7 @@
+export const drivingStoryPermalinks = [
+  "/2026/08/abu-dhabi-driving-rental-car-guide.html",
+  "/2026/08/zayed-airport-car-rental-guide.html",
+  "/2026/08/abu-dhabi-darb-toll-guide.html",
+  "/2026/08/abu-dhabi-mawaqif-parking-guide.html",
+  "/2026/08/abu-dhabi-adnoc-fueling-guide.html"
+] as const;
