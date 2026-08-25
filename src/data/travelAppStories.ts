7db361359618a27000f343271c635412a09e16e1
yoonezra-star <yoonezra@gmail.com>
2026-08-25 20:12:13 +0900

    Add travel app story cluster

---
+++ src/data/travelAppStories.ts
@@ -0,0 +1,7 @@
+export const travelAppStoryPermalinks = [
+  "/2026/08/abu-dhabi-transport-apps-guide.html",
+  "/2026/08/abu-dhabi-taxi-ride-hailing-app-guide.html",
+  "/2026/08/abu-dhabi-offline-map-navigation-guide.html",
+  "/2026/08/zayed-airport-flight-app-guide.html",
+  "/2026/08/abu-dhabi-translation-emergency-app-guide.html"
+] as const;
