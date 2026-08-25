c3099498919ce5f3591d5b1c28748dd7da92d126
yoonezra-star <yoonezra@gmail.com>
2026-08-25 19:12:35 +0900

    Add shopping story cluster data

---
+++ src/data/shoppingStories.ts
@@ -0,0 +1,7 @@
+export const shoppingStoryPermalinks = [
+  "/2026/08/uae-tourist-vat-refund-guide.html",
+  "/2026/08/zayed-airport-vat-refund-guide.html",
+  "/2026/08/abu-dhabi-shopping-mall-comparison.html",
+  "/2026/08/abu-dhabi-souvenir-buying-guide.html",
+  "/2026/08/abu-dhabi-shopping-budget-guide.html"
+] as const;
