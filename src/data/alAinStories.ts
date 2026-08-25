c335e759162a3e5a1b86ea21751dec30d8497d82
yoonezra-star <yoonezra@gmail.com>
2026-08-25 21:47:28 +0900

    Add Al Ain story cluster data

---
+++ src/data/alAinStories.ts
@@ -0,0 +1,7 @@
+export const alAinStoryPermalinks = [
+  "/2026/08/abu-dhabi-al-ain-bus-guide.html",
+  "/2026/08/abu-dhabi-al-ain-rental-car-guide.html",
+  "/2026/08/al-ain-oasis-visit-guide.html",
+  "/2026/08/jebel-hafeet-visit-guide.html",
+  "/2026/08/al-ain-day-trip-from-abu-dhabi-guide.html"
+] as const;
