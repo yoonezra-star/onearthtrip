8a07552190da078933390fcff141301ea56a4328
yoonezra-star <yoonezra@gmail.com>
2026-08-25 19:58:39 +0900

    Add connectivity story cluster

---
+++ src/data/connectivityStories.ts
@@ -0,0 +1,7 @@
+export const connectivityStoryPermalinks = [
+  "/2026/08/zayed-airport-tourist-sim-esim-guide.html",
+  "/2026/08/uae-free-tourist-esim-guide.html",
+  "/2026/08/etisalat-du-tourist-sim-comparison.html",
+  "/2026/08/abu-dhabi-travel-data-usage-guide.html",
+  "/2026/08/abu-dhabi-airport-wifi-roaming-guide.html"
+] as const;
