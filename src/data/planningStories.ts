edaa6443de6548d300e0d10b972891ba4826ae4f
yoonezra-star <yoonezra@gmail.com>
2026-08-25 14:50:32 +0900

    Add Abu Dhabi planning cluster

---
+++ src/data/planningStories.ts
@@ -0,0 +1,7 @@
+export const planningStoryPermalinks = [
+  "/2026/08/abu-dhabi-3-day-itinerary.html",
+  "/2026/08/abu-dhabi-5-day-itinerary.html",
+  "/2026/08/yas-island-planning-guide.html",
+  "/2026/08/saadiyat-downtown-planning-guide.html",
+  "/2026/08/abu-dhabi-family-summer-indoor-itinerary.html"
+] as const;
