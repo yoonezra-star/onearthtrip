2bc1e8ce3a0916a503bae59e79552e3b8e92b7c8
yoonezra-star <yoonezra@gmail.com>
2026-08-25 15:36:42 +0900

    Add downtown Abu Dhabi attraction cluster

---
+++ src/data/downtownStories.ts
@@ -0,0 +1,7 @@
+export const downtownStoryPermalinks = [
+  "/2026/08/qasr-al-watan-visit-guide.html",
+  "/2026/08/qasr-al-hosn-visit-guide.html",
+  "/2026/08/abu-dhabi-corniche-guide.html",
+  "/2026/08/emirates-palace-visit-guide.html",
+  "/2026/08/heritage-village-abu-dhabi-guide.html"
+] as const;
