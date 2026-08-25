69f314c7ed67f521bf95fb98b5d50b0f09462374
yoonezra-star <yoonezra@gmail.com>
2026-08-25 18:51:12 +0900

    Add money guide story cluster

---
+++ src/data/moneyStories.ts
@@ -0,0 +1,7 @@
+export const moneyStoryPermalinks = [
+  "/2026/08/abu-dhabi-cash-card-payment-guide.html",
+  "/2026/08/aed-exchange-guide.html",
+  "/2026/08/zayed-airport-exchange-atm-guide.html",
+  "/2026/08/abu-dhabi-atm-withdrawal-guide.html",
+  "/2026/08/abu-dhabi-tipping-service-charge-guide.html"
+] as const;
